756cef4c62712df8cd7085a77e02deb9fd1fa324
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2021-02-14 14:27:34 +0100

    done

--- APBD_egzamin/Services/HealthSqlDbService.cs
+++ APBD_egzamin/Services/HealthSqlDbService.cs
@@ -1,5 +1,6 @@
 ﻿using APBD_egzamin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,33 +28,56 @@ namespace APBD_egzamin.Services
 
 
             return _context.Medicaments
-                .Where(d => d.IdMedicament == id)
-                .Select(m => new Medicament
-                {
-                    IdMedicament = m.IdMedicament,
-                    Name = m.Name,
-                    Desciption = m.Desciption,
-                    Type = m.Type,
-                    PrescriptionMedicaments = m.PrescriptionMedicaments.ToList()
-                }).ToList();
+                .Where(d => d.IdMedicament == id).Include(m => m.PrescriptionMedicaments).ToList();
             
         }
 
         public bool DeletePatient(int id)
         {
-            Patient patient = _context.Patients.FirstOrDefault(pat => pat.IdPatient == id);
 
-            if (patient != null)
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
             {
-                _context.Attach(patient);
-                _context.Remove(patient);
 
-                _context.SaveChanges();
+                Patient patient = _context.Patients.Include(p => p.Prescriptions)
+                .FirstOrDefault(p => p.IdPatient == id);
 
-                return true;
+                if (patient != null)
+                {
+                    _context.Attach(patient);
+
+                    foreach (var child in patient.Prescriptions.ToList())
+                    {
+                        _context.Attach(child);
+                         List<PrescriptionMedicament> pm = _context.PrescriptionMedicaments
+                            .Where(p => p.IdPrescription == child.IdPrescription)
+                            .ToList();
+
+                    foreach(var pcChild in pm) {
+                        _context.PrescriptionMedicaments.Attach(pcChild);
+                        _context.PrescriptionMedicaments.Remove(pcChild);
+                        _context.SaveChanges();
+                    }
+
+                        _context.Prescriptions.Remove(child);
+                        _context.SaveChanges();
+                }
+                    
+                    _context.Remove(patient);
+                    _context.SaveChanges();
+
+                    transaction.Commit();
+                    return true;
+                }
+
+                
+                return false;
             }
-
-            return false;
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
 
